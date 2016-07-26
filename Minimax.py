e079e0924cd0380edf9c49dc04169d116519941f
root <ascacere92@gmail.com>
2016-07-26 00:34:18 -0500

    agregado algoritmo de minimax

---
+++ Minimax.py
@@ -0,0 +1,27 @@
+
+INFINITEPLUS = 10000
+INFINITENEGATIVE = -10000
+class Artificial_Computation:
+    def __init__(self, gameState):
+        "we receive a game state, so we can generate our minimax function according to the actual board state."
+        self.gameState = gameState
+
+    def minimax(self, node, depth, maximizingPlayer):
+        if(depth == 0 or node.isTerminalNode()):
+            return self.getHeuristicValue()
+
+        if(maximizingPlayer):
+            bestValue = INFINITENEGATIVE
+            for child in node:
+                v = self.minimax(child, depth - 1, False)
+                bestValue = max(v, bestValue)
+            return bestValue
+        else:                 # minimizing player
+            bestValue = INFINITEPLUS
+            for child in node:
+                v = self.minimax(child, depth - 1, True)
+                bestValue = min(bestValue, v)
+            return bestValue
+
+    def getHeuristicValue(self):
+        return self.gameState.getScore()
--- pacman.py
+++ pacman.py
@@ -1,29 +1,35 @@
-import os, sys
+import sys
+
 import pygame
-from pygame.locals import *
-from Sprites.wall import Wall
+
+from Sprites.food import Dot
 from Sprites.ghost import GhostAgent
 from Sprites.pacmanAgent import PacmanAgent
-from Sprites.food import Dot
-if not pygame.font: print ('Warning, fonts disabled')
-if not pygame.mixer: print ('Warning, sound disabled')
-"Main tutorial -> http://www.learningpython.com/2006/03/12/creating-a-game-in-python-using-pygame-part-one/"
+from Sprites.wall import Wall
+
+
+# from pygame.locals import *
+if not pygame.font:
+    print('Warning, fonts disabled')
+if not pygame.mixer:
+    print('Warning, sound disabled')
 
 # Colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (50, 50, 255)
 
+
 class PacmanMain:
     "This class handles the main initialization and creation of the game"
     def __init__(self, width=800, height=530):
-        "initialize pygame"        
+        "initialize pygame"
         pygame.init()
         "set window size"
         self.width = width
         self.height = height
         "Create the screen"
-        self.screen = pygame.display.set_mode((self.width,self.height))
+        self.screen = pygame.display.set_mode((self.width, self.height))
 
         # Set the title of the window
         pygame.display.set_caption('Pacman')
@@ -31,76 +37,70 @@ class PacmanMain:
         # List to hold all the sprites
         self.all_sprite_list = pygame.sprite.Group()
 
-        #list of the ghosts
+        # list of the ghosts
         self.ghosts_list = pygame.sprite.Group()
 
-        # List to hold the dots 
+        # List to hold the dots
         self.all_dots_list = pygame.sprite.Group()
-        
-# Make the walls. (x_pos, y_pos, width, height)
+        # Make the walls. (x_pos, y_pos, width, height)
         self.wall_list = pygame.sprite.Group()
-        
         self.loadLayout("./Game-Layout/layout")
-
         self.pacman.dots_to_eat = self.all_dots_list
         # Create the player paddle object
-        #self.pacman = PacmanAgent(20, 20)
+        # self.pacman = PacmanAgent(20, 20)
         self.pacman.walls = self.wall_list
- 
         self.all_sprite_list.add(self.pacman)
- 
         self.clock = pygame.time.Clock()
-        
+
     def loadLayout(self, filepath):
         """We load the layout from the file"""
-        lineY = 0  #number of the iteration
-        lineX = 0  #actual position in X of the sprite.
-        counterGhosts = 0 #counter of the number of ghosts in the game
+        lineY = 0  # number of the iteration
+        lineX = 0  # actual position in X of the sprite.
+        counterGhosts = 0  # counter of the number of ghosts in the game
         f = open(filepath, 'r')
         for line in f:
-            lineX = 0  #actual position in X of the sprite.        
+            lineX = 0  # actual position in X of the sprite
             line = line.strip()
-            #print(line)
+            # print(line)
             for c in line:
                 if(c == "@"):
-                    wall = Wall(lineX,lineY*40, 35, 35)
+                    wall = Wall(lineX, lineY*40, 35, 35)
                     self.wall_list.add(wall)
                     self.all_sprite_list.add(wall)
                 elif(c == "."):
-                    dot = Dot(lineX+10,lineY*40+15, 10, 10)
+                    dot = Dot(lineX+10, lineY*40+15, 10, 10)
                     self.all_sprite_list.add(dot)
                     self.all_dots_list.add(dot)
                 elif(c == "P"):
-                    self.pacman = PacmanAgent(lineX+20, lineY*40)
+                    self.pacman = PacmanAgent(lineX+10, lineY*40)
                     self.pacman.walls = self.wall_list
                     self.all_sprite_list.add(self.pacman)
                 elif(c == "G"):
                     print(counterGhosts)
                     if(counterGhosts == 0):
-                        ghost = GhostAgent(lineX+10,lineY*40,"blinky")
+                        ghost = GhostAgent(lineX+10, lineY*40, "blinky")
                         ghost.walls = self.wall_list
                         self.ghosts_list.add(ghost)
                         self.all_sprite_list.add(ghost)
                     elif(counterGhosts == 1):
-                        ghost = GhostAgent(lineX+10,lineY*40,"clyde")
+                        ghost = GhostAgent(lineX+10, lineY*40, "clyde")
                         self.ghosts_list.add(ghost)
                         self.all_sprite_list.add(ghost)
                         ghost.walls = self.wall_list
                     elif(counterGhosts == 2):
-                        ghost = GhostAgent(lineX+10,lineY*40,"pinky")
+                        ghost = GhostAgent(lineX+10, lineY*40, "pinky")
                         self.ghosts_list.add(ghost)
                         self.all_sprite_list.add(ghost)
                         ghost.walls = self.wall_list
                     elif(counterGhosts == 3):
-                        ghost = GhostAgent(lineX+10,lineY*40,"red")
+                        ghost = GhostAgent(lineX+10, lineY*40, "red")
                         self.all_sprite_list.add(ghost)
                         ghost.walls = self.wall_list
-                    counterGhosts+=1
-                    
+                    counterGhosts += 1
                 elif(c == "o"):
                     pass
                 lineX += 40
-            lineY+=1
+            lineY += 1
 
     def mainLoop(self):
         "Main loop of the game"
@@ -110,8 +110,8 @@ class PacmanMain:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    self.pacman.image = pygame.transform.scale(self.pacman.image, (30,30))
-                    if event.key == pygame.K_LEFT:
+                    self.pacman.image = pygame.transform.scale(self. pacman.image, (20, 20))
+                    if event.kecy == pygame.K_LEFT:
                         self.pacman.image = pygame.image.load("Images/pacman_left.png").convert_alpha()
                         self.pacman.changespeed(-3, 0)
                     elif event.key == pygame.K_RIGHT:
@@ -137,6 +137,8 @@ class PacmanMain:
             self.all_sprite_list.draw(self.screen)
             pygame.display.flip()
             self.clock.tick(60)
+
+
 def main():
     MainWindow = PacmanMain()
     MainWindow.mainLoop()
